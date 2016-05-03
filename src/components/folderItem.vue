c4cc6d6b856f3714def34386357ad67ea92b3293
Rachel Chen <me@rachelchen.me>
2016-05-03 14:23:17 -0700

    ordered list magically fix the indentation bug

--- src/components/folderItem.vue
+++ src/components/folderItem.vue
@@ -27,13 +27,13 @@
 					<a class="btn gray h3" @click="flipMenuAndDescription" v-if="folder.mutable || folder.displayName.toLowerCase() == 'inbox'">
 						&#8942;
 					</a>
-					<a class="btn gray h4" @click="showTruncateFolder" v-if="st.hideInMoveOptions.indexOf(folder.displayName.toLowerCase()) !== -1">
+					<a class="btn gray h3" @click="showTruncateFolder" v-if="st.hideInMoveOptions.indexOf(folder.displayName.toLowerCase()) !== -1">
 						&#8709;
 					</a>
 				</div>
 			</div>
 		</div>
-		<ul class="m0"><folder-item v-for="folder in folder.child" :folder="folder"></folder-item></ul>
+		<ol class="m0"><folder-item v-for="folder in folder.child" :folder="folder"></folder-item></ol>
 		<modal :show.sync="truncateModal">
 			<h4 slot="header">Truncate a Folder</h4>
 			<span slot="body">
