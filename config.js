42b64ed0c169a67b636c99ca4b6b7653a443a4c5
Rachel Chen <me@rachelchen.me>
2016-05-20 14:09:31 -0700

    remove useless config

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
 	apiVersion: 2,
 	port: require('./config.json').port,
-	graylog: require('./config.json').graylog || null,
 	apiEndpoint: require('./config.json').apiEndpoint,
 	siteURL: require('./config.json').siteURL
 }
