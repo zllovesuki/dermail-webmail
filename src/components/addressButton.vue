43e6ad5e06fa580a844cf6f9630383d5d255d42c
Rachel Chen <me@rachelchen.me>
2016-05-30 20:48:56 -0700

    use popup for addresses

--- src/components/addressButton.vue
+++ src/components/addressButton.vue
@@ -2,7 +2,7 @@
 	<span>
 		<span class="btn mxn1 black h6 muted not-clickable">{{ originText }}: </span>
 		<template v-for="address in origin">
-			<a class="muted h6 bold btn mxn1 {{ st.color }}" @click="flipContent" title="{{address.account}}@{{address.domain}}">{{ address | nameOrEmail }}</a>
+			<a class="muted h6 bold btn mxn1 {{ st.color }}" @click="popup" data-name="{{address.friendlyName}}" data-email="{{address.account}}@{{address.domain}}">{{ address | nameOrEmail | excerpt 15 }}</a>
 		</template>
 	</span>
 </template>
@@ -28,11 +28,17 @@ module.exports = {
 		}
 	},
 	methods: {
-		flipContent: function(e) {
-			var flip = e.target.title;
-			var was = e.target.innerHTML;
-			e.target.innerHTML = flip;
-			e.target.setAttribute('title', was);
+		popup: function(e) {
+			var msg = '';
+			var name = e.target.attributes['data-name'].value;
+			var email = e.target.attributes['data-email'].value
+			if (name.length > 1) {
+				msg += '<span>' + name + '</span>';
+			}
+			if (email.length > 1) {
+				msg += ' <span class="bold" style="word-wrap: break-word;">&lt;' + email + '></span>';
+			}
+			this.st.alert.alert(msg);
 		}
 	}
 }
