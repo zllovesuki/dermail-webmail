708e6f72211ed7833cc24811226be6921c2cec9b
Rachel Chen <me@rachelchen.me>
2016-07-22 18:57:20 -0700

    oops es6 syntax

--- src/lib/vuex/store.js
+++ src/lib/vuex/store.js
@@ -1,13 +1,11 @@
 var	Vue = require('vue'),
-	Vuex = require('vuex'),
-	state = require('./state'),
-	mutations = require('./mutations');
+	Vuex = require('vuex');
 
 Vue.use(Vuex);
 
 var store = new Vuex.Store({
-	state,
-	mutations
+	state: require('./state'),
+	mutations: require('./mutations')
 })
 
 module.exports = store;
